54eabc1d5788f76999fd6bdb9e91eb4f0a12ce88
Luiza <35385082+lugim@users.noreply.github.com>
2023-03-01 19:31:49 -0500

    portal effect

---
+++ VARLabCodeJamGolfCourse/Assets/Scripts/PortalCollider.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+
+
+        if (this.gameObject.tag == "Portal1")
+        {
+
+            var portalOut = GameObject.FindGameObjectWithTag("Portal2");
+            other.transform.position = portalOut.transform.position;
+
+        }
+
+
+        if(this.gameObject.tag == "Portal2")
+        {
+            var portalOut = GameObject.FindGameObjectWithTag("Portal1");
+
+            other.transform.position = portalOut.transform.position;
+        }
+    }
+
+}
